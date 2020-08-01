626292344ca2147184cfa9e4f7bcd64178de5d18
lajesticvantrashell <hamishhuggard@protonmail.com>
2020-08-01 17:28:10 +1200

    read RP data CSVs

--- app.py
+++ app.py
@@ -8,27 +8,43 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import re
+from glob import glob
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+##################################
+###       DEMOGRAPHICS         ###
+##################################
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+for path in glob('./rp_survey_data/*.csv'):
+    data = pd.read_csv(path, sep='\t')
+    title = data.columns[0]
+    print(title)
+    print(data)
+
+
+##################################
+###         WORLD MAP          ###
+##################################
+
+# source: https://plotly.com/python/bubble-maps/
+
+df = px.data.gapminder()
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1('Effective Altruism Dashboard'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    # html.Div('''
+    #     Dash: A web application framework for Python.
+    # '''),
 
     dcc.Graph(
         id='example-graph',
