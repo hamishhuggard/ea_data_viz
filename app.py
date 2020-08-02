96bd1399bd2ebb7671720fe368c73856f52d1f78
lajesticvantrashell <hamishhuggard@protonmail.com>
2020-08-02 18:25:55 +1200

    OP grants sankey diagram

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import re
@@ -97,6 +98,8 @@ map_fig = px.scatter_geo(countries, locations="iso_alpha", #color="continent",
 
 # want source -> cause area -> charity
 funding = pd.DataFrame(columns=['Source', 'Cause Area', 'Organization', 'Amount'])
+
+# Parse open philanthropy grants
 op_grants = pd.read_csv('./openphil_grants.csv')
 op_grants = op_grants[['Organization Name', 'Focus Area', 'Amount']]
 op_grants.rename(columns={
@@ -106,7 +109,35 @@ op_grants.rename(columns={
 }, inplace=True)
 op_grants['Source'] = 'Open Philanthropy'
 funding = funding.append(op_grants)
-print(funding)
+
+# Get a list of all funding-related entities
+entities = set()
+for col in ['Source', 'Cause Area', 'Organization']:
+    entities.update(funding[col])
+entities = list(entities)
+
+# Convert financial inputs and outputs into indices
+entity2idx = {x: i for i,x in enumerate(entities)}
+sources = list(funding['Source'].map(entity2idx))
+targets = list(funding['Cause Area'].map(entity2idx))
+
+# Parse funding amounts
+funding['Amount'] = funding['Amount'].apply(lambda x: int(x[1:].replace(',', '') if type(x)==str else 0)).astype('int')
+
+# Create Sankey diagram
+funding_fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = entities,
+      color = "blue"
+    ),
+    link = dict(
+      source = sources, # indices correspond to labels, eg A1, A2, A2, B1, ...
+      target = targets,
+      value = funding['Amount']
+  ))])
 
 
 ##################################
@@ -115,6 +146,10 @@ print(funding)
 
 app.layout = html.Div(children=[
     html.H1('Effective Altruism Dashboard'),
+
+    total_div,
+
+    dcc.Graph(figure=funding_fig),
 
     # html.Div('''
     #     Dash: A web application framework for Python.
@@ -130,7 +165,8 @@ app.layout = html.Div(children=[
 
         html.Div(demo_pies, style={'columnCount': 2, 'float': 'left', 'padding': '0px 0px 0px 200px',
         'width': '30%', 'display': 'inline-block'}),
-    ])
+    ]),
+
 ])
 
 if __name__ == '__main__':
