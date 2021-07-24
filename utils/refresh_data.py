6b510fc264a7fd1c2f3bb1e20535a9ee7a06a5db
hamishhuggard <hamishhuggard@gmail.com>
2021-07-25 01:37:31 +1200

    I guess add  this as well

---
+++ utils/refresh_data.py
@@ -0,0 +1,19 @@
+import utils.data_scraping as data_scraping
+import os
+import time
+
+most_recent_refresh = None
+
+def refresh_data():
+    global most_recent_refresh
+    if most_recent_refresh and time.time() - most_recent_refresh < 60*60:
+        return
+    most_recent_refresh = time.time()
+    new_op_data = data_scraping.download_op_grants()
+    new_op_data = new_op_data.text
+    print('latest OP grant: ', new_op_data.split('\n')[1])
+    op_data_path = os.path.abspath('./data/openphil_grants.csv')
+    with open(op_data_path, 'w') as f:
+        f.write(new_op_data)
+
+
