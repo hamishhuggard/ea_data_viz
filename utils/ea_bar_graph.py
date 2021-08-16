6bc2e41fcede453f23775cd7b11f9d236d992c73
hamishhuggard <hamishhuggard@gmail.com>
2021-08-16 19:28:35 +1200

    Make the demographics responsive

--- utils/ea_bar_graph.py
+++ utils/ea_bar_graph.py
@@ -8,42 +8,38 @@ class EABarGraph(dcc.Graph):
 
     def __init__(self, df, height, title=None):
 
-        self.bar = px.bar(
+        if 'text' in df.columns:
+            text_col = 'text'
+        else:
+            text_col = 'y'
 
+        if 'hover' in df.columns:
+            hover_col = 'hover'
+        else:
+            hover_col = 'x'
+
+        self.bar = px.bar(
             df,
             y='x',
             x='y',
+            text=text_col,
             title=title,
             height=height,
             orientation='h',
-
-            # Don't provide any hover data
-            hover_data={
-                'x': False,
-                'y': False,
-            },
-
-            # These are the labels shown in the hoverdata:
-            # labels={
-            #     'label': title,
-            #     'Percent': 'Percentage',
-            # },
         )
 
         self.bar.update_traces(
             # The color of the bars
             marker_color="#0c869b",
-            hoverinfo='skip',
 
-            # Template for hovertext
-            # hovertemplate = '%{hover} %{Percent:$.2f} %{label}<extra></extra>',
+            hovertext = df[hover_col],
+            hovertemplate = '%{hovertext}<extra></extra>',
         )
 
         self.bar.update_xaxes(side='top')
 
         self.bar.update_layout(
             margin=dict(l=0, r=0, t=30, b=0),
-            hovermode=False,
             xaxis=dict(
                 title='',
                 fixedrange=True
@@ -54,9 +50,8 @@ class EABarGraph(dcc.Graph):
             ),
             # title_x=0.5,
             font=dict(
-                family="Roboto Slab",
-                size=15,
-                # color="#0c869b"
+                family="Raleway",
+                size=12,
             )
         )
 
