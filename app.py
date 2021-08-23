918655b48be23f4639ce7ce791403d9312e1c322
hamishhuggard <hamishhuggard@gmail.com>
2021-08-23 23:22:59 +1200

    Hide openphil data

--- app.py
+++ app.py
@@ -6,11 +6,11 @@
 import dash
 import dash_html_components as html
 from sections.donations import content as donations
-from sections.demographics import content as demographics
-from sections.growth import content as growth
-from sections.geography import content as geography
+from sections.demographics import Demographics
+from sections.growth import Growth
+from sections.geography import Geography
 from sections.title import content as title
-from sections.open_phil import content as open_phil
+# from sections.open_phil import content as open_phil
 from utils.refresh_data import refresh_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -19,18 +19,18 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Effective Altruism Data Viewer'
 server = app.server
 
-refresh_data()
+# refresh_data()
 
 # def serve_layout():
 app.layout = html.Div(
 #     return html.Div(
         [
             title,
-            demographics,
-            geography,
-            open_phil,
+            Demographics(),
+            Geography(),
+            # open_phil,
             donations,
-            growth,
+            Growth(),
         ],
         className = 'scroll-snapper',
     )
@@ -38,4 +38,4 @@ app.layout = html.Div(
 # app.layout = serve_layout
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
