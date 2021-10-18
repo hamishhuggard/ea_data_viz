7f354d2bff0f31b052f9245e44ab7bcade4f91db
hamishhuggard <hamishhuggard@gmail.com>
2021-10-19 10:14:35 +1300

    Move line lables to left above

--- plots/line.py
+++ plots/line.py
@@ -42,17 +42,7 @@ class Line(dcc.Graph):
                 )
             )
 
-            if log_y:
-                fig.update_layout(
-                    yaxis_type="log",
-                )
-
-            fig.update_layout(
-                title=title,
-                showlegend=False,
-            )
-
-            val_df = val_df.loc[ val_df['value'].notnull() ].reset_index()
+            val_df = val_df.loc[ val_df[y].notnull() ].reset_index()
             last_row = val_df.iloc[len(val_df)-1]
             last_hover = last_row['hover']
 
@@ -63,56 +53,39 @@ class Line(dcc.Graph):
                 marker=dict(
                     color="#0c869b",
                     size=10,
-                    #mode_size[i]
                 ),
                 hovertext = [last_hover],
                 hovertemplate = '%{hovertext}<extra></extra>',
             ))
 
             annotations.append(dict(
-                #xref='paper',
-                #yref='paper',
-                x=last_row['year'],
-                y=last_row['value'],
-                xanchor='left',
-                yanchor='middle',
+                x=last_row[x],
+                y=last_row[y],
+                xanchor='right',
+                yanchor='bottom',
                 text=f' {val}',
                 font={
-                #    'family': 'Arial',
                     'size': 13,
                 },
                 showarrow=False,
             ))
 
-        fig.update_layout(
-
-            annotations=annotations,
-            #margin=dict(l=0, r=0, t=30, b=0),
-            margin=dict(l=0, r=0, t=0, b=0),
-            title_x=0.5,
-
-            # autosize = True,
-
-            # Top-left corner:
-
-            legend = dict(
-                yanchor="top",
-                y=0.99,
-                xanchor="left",
-                x=0.01,
-                title_text='',
+        if log_y:
+            fig.update_layout(
+                yaxis_type="log",
             )
 
-            # Below:
+        fig.update_layout(
+            title=title,
+            showlegend=False,
+        )
 
-            # legend = {
-            #       'xanchor': "center",
-            #       'yanchor': "top",
-            #       'y': -0.3, # play with it
-            #       'x': 0.5,   # play with it
-            #       'title_text': '',
-            # }
 
+        top_margin = 40 if title else 0
+        fig.update_layout(
+            annotations=annotations,
+            margin=dict(l=0, r=0, t=top_margin, b=0),
+            title_x=0.5,
         )
 
         super().__init__(
