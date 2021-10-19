a298d1fa0628f41985550c38d5060c33314fba72
hamishhuggard <hamishhuggard@gmail.com>
2021-10-19 15:04:34 +1300

    Informative hovertext

--- sections/forum.py
+++ sections/forum.py
@@ -126,89 +126,180 @@ def post_counts(forum_df):
 
     forum_df['Unit'] = 1
     forum_df['Count'] = forum_df['Unit'].cumsum()
-    label = forum_df['Count'].tolist()[-1]
-    label = f'{label:,} Posts'
-    forum_df['label'] = label
-
     forum_df['posted_at'] = forum_df['posted_at'].dt.date
-    posted_at_groups = forum_df.groupby('posted_at')
-    post_counts_df = pd.DataFrame({
-        'posted_at': forum_df['posted_at'].unique()
-    })
-    #titles_string = posted_at_groups['title'].transform(lambda x: ', '.join(x))
-    #titles_string = titles_string.drop_duplicates().tolist()
-    #post_counts_df['titles'] = titles_string
-    post_counts_df['titles'] = posted_at_groups['title'].apply(list).tolist()
-    post_counts_df['new_posts'] = posted_at_groups['Unit'].sum().tolist()
-    post_counts_df['new_words'] = posted_at_groups['wordcount'].sum().tolist()
-    post_counts_df['total_posts'] = posted_at_groups['Count'].last().tolist()
-    print(post_counts_df)
-
-    def hover(row):
-
-        title = row['title']
-        posted_at = row['posted_at_readable']
-        wordcount = row['wordcount']
-        karma = row['karma']
-        comments = row['comments']
-
-        result = ''
-        result += f"<b>{title}</b>"
-        result += f"<br>{authors}"
-        result += f"<br>Posted {posted_at}"
-        result += f"<br>{karma} karma, {comments} comments"
-
-        return result
-
-    #forum_df['hover'] = forum_df.apply(hover, axis=1)
-
-    post_count_graph = Line(
-        forum_df,
-        x='posted_at',
-        y='Count',
-        x_title = '',
-        y_title = '',
-        hover = 'hover',
-        title = 'Post Count',
-    )
+    forum_df['cumulative_word_count'] = forum_df['wordcount'].cumsum()
 
     authors = set()
     def new_authors(author_string):
         author_list = author_string.split(',')
         author_list = [ author.strip() for author in author_list ]
-        authors_before = len(authors)
-        authors.update(author_list)
-        authors_after = len(authors)
-        return authors_after - authors_before
+        new_authors = []
+        for author in author_list:
+            if author not in authors:
+                new_authors.append(author)
+                authors.add(author)
+        return new_authors
+
     forum_df['new_authors'] = forum_df['authors'].apply(new_authors)
-    forum_df['author_count'] = forum_df['new_authors'].cumsum()
+    forum_df['new_author_count'] = forum_df['new_authors'].apply(len)
+    forum_df['author_count'] = forum_df['new_author_count'].cumsum()
+
+    posted_at_groups = forum_df.groupby('posted_at')
+    forum_by_day_df = pd.DataFrame({
+        'posted_at': forum_df['posted_at'].unique(),
+        'posted_at_readable': forum_df['posted_at_readable'].unique(),
+    })
+
+    # new posts
+
+    forum_by_day_df['titles'] = posted_at_groups['title'].apply(list).tolist()
+    forum_by_day_df['new_posts'] = posted_at_groups['Unit'].sum().tolist()
+    forum_by_day_df['total_posts'] = posted_at_groups['Count'].last().tolist()
+
+    label = forum_df['Count'].tolist()[-1]
+    label = f'{label:,} Posts'
+    forum_by_day_df['new_posts_label'] = label
+
+    def new_posts_hover(row):
+
+        new_posts = row['new_posts']
+        total_posts = row['total_posts']
+        posted_at = row['posted_at_readable']
+        titles = row['titles']
+
+        max_displayed_titles = 10
+        if len(titles) > max_displayed_titles:
+            titles = titles[:max_displayed_titles] + ['...']
+
+        result = ''
+        result += f"<b>{posted_at}</b>"
+        #result += f"<br><b>{total_posts} total posts</b>"
+        #result += f"<br><b>{new_posts} new posts:</b>"
+        result += f"<br><b>{new_posts} new posts ({total_posts:,} total):</b>"
+        for title in titles:
+
+            max_title_length = 50
+            if len(title) > max_title_length:
+                title = title[:max_title_length] + '...'
+            result += f"<br>{title}"
+
+        return result
+
+    forum_by_day_df['new_posts_hover'] = forum_by_day_df.apply(new_posts_hover, axis=1)
+
+
+    # new authors
+
+    forum_by_day_df['new_authors'] = posted_at_groups['new_authors'].sum().tolist()
+    forum_by_day_df['new_author_count'] = posted_at_groups['new_author_count'].sum().tolist()
+    forum_by_day_df['author_count'] = posted_at_groups['author_count'].last().tolist()
+
     label = forum_df['author_count'].tolist()[-1]
     label = f'{label:,} Unique Authors'
-    forum_df['label'] = label
+    forum_by_day_df['new_authors_label'] = label
+
+    def new_authors_hover(row):
+
+        new_authors = row['new_authors']
+        new_authors_count = row['new_author_count']
+        total_authors = row['author_count']
+        posted_at = row['posted_at_readable']
+
+        max_displayed_authors = 10
+        if len(new_authors) > max_displayed_authors:
+            new_authors = new_authors[:max_displayed_authors] + ['...']
+
+        result = ''
+        #result += f"<b>{posted_at}</b>"
+        #result += f"<br><b>{total_authors} total authors</b>"
+        #result += f"<b>{posted_at} - {total_authors} total authors</b>"
+        #result += f"<br><b>{new_authors_count} new authors:</b>"
+        result += f"<b>{posted_at}</b>"
+        result += f"<br><b>{new_authors_count} new authors ({total_authors:,} total):</b>"
+        for author in new_authors:
+
+            max_author_length = 50
+            if len(author) > max_author_length:
+                author = author[:max_author_length] + '...'
+            result += f"<br>{author}"
+
+        return result
+
+    forum_by_day_df['new_authors_hover'] = forum_by_day_df.apply(new_authors_hover, axis=1)
+
+
+    # word count
+
+
+    forum_by_day_df['new_words'] = posted_at_groups['wordcount'].sum().tolist()
+    forum_by_day_df['total_words'] = posted_at_groups['cumulative_word_count'].last().tolist()
+
+    label = forum_by_day_df['total_words'].tolist()[-1]
+    label = f'{label:,} Words'
+    forum_by_day_df['word_count_label'] = label
+
+    def word_count_hover(row):
+
+        new_words = row['new_words']
+        total_words = row['total_words']
+        posted_at = row['posted_at_readable']
+
+        result = ''
+        result += f"<b>{posted_at}</b>"
+        result += f"<br>{total_words:,} total words"
+        result += f"<br>{new_words:,} new words"
+        #result += f"<b>{posted_at} - {total_words:,} total words"
+        #result += f"<br>{new_words:,} new words"
+
+        return result
+
+    forum_by_day_df['word_count_hover'] = forum_by_day_df.apply(word_count_hover, axis=1)
+
+
+
+    # New posts line plot
+
+    post_count_graph = Line(
+        forum_by_day_df,
+        x='posted_at',
+        y='total_posts',
+        x_title = '',
+        y_title = '',
+        hover = 'new_posts_hover',
+        title = 'Post Count',
+        label = 'new_posts_label',
+    )
+
+    # New authors line plot
+
+    unique_authors_df = forum_by_day_df.loc[ forum_by_day_df['new_author_count'] > 0 ]
 
     author_count_graph = Line(
-        forum_df,
+        unique_authors_df,
         x='posted_at',
         y='author_count',
         x_title = '',
         y_title = '',
-        hover = 'hover',
+        hover = 'new_authors_hover',
         title = 'Unique Author Count',
+        label = 'new_authors_label',
     )
 
-    forum_df['cumulative_word_count'] = forum_df['wordcount'].cumsum()
-    label = forum_df['cumulative_word_count'].tolist()[-1]
-    label = f'{label:,} Words'
-    forum_df['label'] = label
+
+
+    # New words line plot
+
+    new_words_df = forum_by_day_df.loc[ forum_by_day_df['new_words'] > 0 ]
 
     word_count_graph = Line(
-        forum_df,
+        new_words_df,
         x='posted_at',
-        y='cumulative_word_count',
+        y='total_words',
         x_title = '',
         y_title = '',
-        hover = 'hover',
+        hover = 'word_count_hover',
         title = 'Cumulative Word Count',
+        label = 'word_count_label',
     )
 
     return [
