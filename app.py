762859c429cd3c67de2337a86f267627d8dbbf6d
y-arjun-y <arjyadav7@gmail.com>
2021-11-08 08:21:50 +0400

    added meta tags for twitter card

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ app = dash.Dash(
     __name__,
     meta_tags = [
         {
+            'og:title': 'Effective Altruism Data',
+            "og:url": "https://effectivealtruismdata.com",
+            "og:site_name": "Effective Altruism Data",
+            "og:image": "https://i.ibb.co/mqbpdXW/eadata.png",
+            "og:image:width": "1440",
+            "og:image:height": "630",
+            "twitter:card": "summary_large_image",
             'name': 'viewport',
             'content': 'width=device-width, initial-scale=1.0',
         }
@@ -63,4 +70,4 @@ def sidebar(x):
 
 if __name__ == '__main__':
     #app.run_server(debug=True)
-    app.run_server(debug=False)
+    app.run_server(host="0.0.0.0", debug=True)
